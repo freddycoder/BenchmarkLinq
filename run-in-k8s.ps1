269746da5d8aaa3654464f8f0f1e696cda6ec6f8
Frédéric Jacques <gary.bob88@gmail.com>
2022-05-11 22:37:34 -0400

    remove not working codes

--- run-in-k8s.ps1
+++ run-in-k8s.ps1
@@ -5,9 +5,3 @@ kubectl create namespace $namespace
 
 # create the newman pod
 kubectl apply -f pod.yaml -n $namespace
-
-# wait for the newman pod to be ready
-kubectl wait --for=condition=Ready -n $namespace pod/benchmark-csharp --timeout=600s
-
-# attach to the newman pod
-kubectl attach pod/benchmark-csharp -c benchmark-csharp -n $namespace
